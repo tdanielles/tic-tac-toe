76acd819b18655b9373855e14e62c5a5fc739193
Trisha Sia <trishasia@Trishas-MBP.hitronhub.home>
2022-06-07 18:32:51 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,143 @@
+const playerFactory = (name, sign) => {
+    this.name = name;
+    this.sign = sign;
+
+    const getSign = () => {
+        return sign;
+    };
+
+    const getName = () => {
+        return name;
+    };
+    
+    return {getName, getSign};
+};
+
+const gameBoard = (() => {
+    const board = ["","","","","","","","",""];
+
+    const setField = (index, sign) => {
+        if(index > board.length) return;
+        board[index] = sign;
+    };
+
+    const getField = (index) => {
+        if(index > board.length) return;
+        return board[index];
+    };
+
+    const reset = () => {
+        for(let i=0; i<board.length; i++) {
+            board[i] = "";
+        }
+    };
+
+    return {setField, getField, reset};
+})();
+
+// UI stuff
+const displayController = (() => {
+    const fieldElements = document.querySelectorAll(".field");
+    const messageElement = document.getElementById("message");
+    const restartButton = document.getElementById("restart-button");
+
+    fieldElements.forEach((field) =>
+        field.addEventListener('click', (e) => {
+            if (gameController.getIsOver() || e.target.textContent !== "") return;
+            gameController.playRound(parseInt(e.target.dataset.index));
+            updateGameboard();
+        })
+    );
+
+    restartButton.addEventListener('click', (e) => {
+        gameBoard.reset();
+        gameController.reset();
+        updateGameboard();
+        setMessageElement("Player 1's Turn");
+    });
+
+    const updateGameboard = () => {
+        for (let i=0; i<fieldElements.length; i++) {
+            fieldElements[i].textContent = gameBoard.getField(i);
+        }
+    };
+
+    const setResultMessage = (winner) => {
+        if (winner === "Draw") {
+            setMessageElement("It's a draw!");
+        } else {
+            setMessageElement(`Player ${winner} has won!`);
+        }
+    };
+
+    const setMessageElement = (message) => {
+        messageElement.textContent = message;
+    };
+
+    return {setResultMessage, setMessageElement};
+})();
+
+// Tic-tac-toe game
+const gameController = (() => {
+    const playerOne = playerFactory("1", "O");
+    const playerTwo = playerFactory("2", "X");
+    let round = 1;
+    let isOver = false;
+
+    const playRound = (fieldIndex) => {
+        gameBoard.setField(fieldIndex, getCurrentPlayerSign());
+        if (checkWinner(fieldIndex)) {
+            displayController.setResultMessage(getCurrentPlayerName());
+            isOver = true;
+            return;
+        }
+        if (round === 9) {
+            displayController.setResultMessage("Draw");
+            isOver = true;
+            return;
+        }
+        round++;
+        displayController.setMessageElement(
+            `Player ${getCurrentPlayerName()}'s Turn`
+        );
+    };
+    
+    const getCurrentPlayerSign = () => {
+        return round % 2 === 1? playerOne.getSign() : playerTwo.getSign();
+    };
+
+    const getCurrentPlayerName = () => {
+        return round % 2 === 1? playerOne.getName() : playerTwo.getName();
+    };
+
+    const checkWinner = (fieldIndex) => {
+        const winConditions = [
+            [0, 1, 2], // straight horizontals
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6], // straight verticals
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8], // straight diagonals
+            [2, 4, 6],
+        ];
+
+        return winConditions.filter((combination) => combination.includes(fieldIndex))
+            .some((possibleCombination) =>
+                possibleCombination.every(
+                    (index) => gameBoard.getField(index) === getCurrentPlayerSign()
+                )
+            );
+    };
+
+    const getIsOver = () => {
+        return isOver;
+    };
+
+    const reset = () => {
+        round = 1;
+        isOver = false;
+    };
+
+    return {playRound, getIsOver, reset};
+})();
